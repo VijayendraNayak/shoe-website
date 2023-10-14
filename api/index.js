745b841d9c4541be5b913cc704fb0143e7b95114
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-10-14 13:57:34 +0530

    mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
-const express=require("express");
-const app=express();
-app.listen(3000,()=>{
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Connected to mongodb")
+}).catch((err) => {
+  console.log(err)
+});
+const app = express();
+app.listen(3000, () => {
   console.log("server listening at port 3000")
 })
