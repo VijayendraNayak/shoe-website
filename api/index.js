911825002ad138683d6c9d30712885579a717104
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-10-15 15:34:19 +0530

    signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,5 +14,6 @@ const app = express();
 app.listen(3000, () => {
   console.log("server listening at port 3000")
 })
-
+app.use(express.json())
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,27 +4,27 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-red-100'>
+    <header className='bg-orange-100'>
         <div className='flex justify-between p-3 max-w-6xl mx-auto'>
         <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-red-300'>Shoe</span>
-            <span className='text-red-500'>Shop</span>
+            <span className='text-orange-300'>Shoe</span>
+            <span className='text-orange-500'>Shop</span>
         </h1>
         </Link>
-        <form className='bg-red-200 rounded-lg flex items-center px-3 '>
+        <form className='bg-orange-200 rounded-lg flex items-center px-3 '>
             <input  type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64 ' />
-            <FaSearch className='text-red-600'></FaSearch>
+            <FaSearch className='text-orange-600'></FaSearch>
         </form>
         <ul className='flex  gap-4'>
             <Link to='/'>
-            <li className='hidden sm:inline  hover:underline text-red-700'>Home</li>
+            <li className='hidden sm:inline  hover:underline text-orange-700'>Home</li>
             </Link>
             <Link to='/about'>
-            <li className='hidden sm:inline  hover:underline text-red-700'>About</li>
+            <li className='hidden sm:inline  hover:underline text-orange-700'>About</li>
             </Link>
             <Link to='/sign-in'>
-            <li className=' hover:underline text-red-700'>Sign in</li>
+            <li className=' hover:underline text-orange-700'>Sign in</li>
             </Link>
         </ul>
         </div>
