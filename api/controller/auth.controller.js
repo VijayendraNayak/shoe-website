f70d83f8493cfc3ef6a451923851e8b11f974c86
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-10-15 16:24:14 +0530

    middlware

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.modles.js";
 import bcryptjs from "bcryptjs";
-export const signup=async(req,res)=>{
+import { errorhandler } from "../utils/error.js";
+export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashedpassword=bcryptjs.hashSync(password,10)
     const newuser=new User({username,email,password:hashedpassword})
@@ -8,6 +9,7 @@ export const signup=async(req,res)=>{
         await newuser.save();
         res.status(201).json("User created successfully");
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
+        // next(errorhandler(550,"message"))
     }
 }
--- api/index.js
+++ api/index.js
@@ -15,5 +15,14 @@ app.listen(3000, () => {
   console.log("server listening at port 3000")
 })
 app.use(express.json())
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "server error";
+  return res.status(statusCode).json({
+    succeses: false,
+    statusCode,
+    message
+  })
+})
