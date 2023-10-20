b5c008c14a000307ff3a63718d5239f1ebd3f688
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-10-20 21:20:58 +0530

    profile ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector } from "react-redux";
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
+    <div className="p-7 max-w-lg mx-auto">
+      <form className="flex flex-col gap-4">
+        <img
+          className="rounded-full w-16 h-16 self-center object-cover"
+          src={currentUser.photo}
+          alt="profileimage"
+        />
+        <input
+          className="p-3 rounded-lg bg-orange-50"
+          placeholder="username"
+          type="text"
+          id="username"
+        />
+        <input
+          className="p-3 rounded-lg bg-orange-50"
+          placeholder="email"
+          type="text"
+          id="email"
+        />
+        <input
+          className="p-3 rounded-lg bg-orange-50"
+          placeholder="password"
+          type="text"
+          id="password"
+        />
+        <button className="p-3 rounded-lg bg-orange-500 text-white">
+          UPDATE
+        </button>
+      </form>
+
+      <div className="flex justify-between mt-4">
+        <span className="text-red-500 cursor-pointer hover:text-red-700">
+          Delete account
+        </span>
+        <span className="text-red-500 cursor-pointer hover:text-red-700">
+          SignOut
+        </span>
+
+      </div>
+    </div>
+  );
 }
