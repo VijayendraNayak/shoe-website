753c11f2f42d045e2e7e5b79daa2758d7e6c2631
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-10-20 16:17:39 +0530

    firebase

--- api/models/User.modles.js
+++ api/models/User.modles.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    photo:{
+        type:String,
+        default:"https://imgs.search.brave.com/PzngAPChR2G1EghyNpeb6l57-C-wwF0B_VXbrqZORFw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA2LzE5LzI2LzQ2/LzM2MF9GXzYxOTI2/NDY4MF94MlBCZEdM/RjU0c0ZlN2tUQnRB/dlpuUHlYZ3ZhUncw/WS5qcGc"
+    }
 },{timestamps:true})
 
 const User=mongoose.model('User',userSchema)
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYWKwx5NVrhjfnJGTIoZozMDhcxgDH06k",
+  authDomain: "shoeshopi.firebaseapp.com",
+  projectId: "shoeshopi",
+  storageBucket: "shoeshopi.appspot.com",
+  messagingSenderId: "512698362626",
+  appId: "1:512698362626:web:2f34080fb3905154ea301b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
