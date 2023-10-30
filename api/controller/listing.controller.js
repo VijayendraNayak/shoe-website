eea10ad02e81b60087897698e13974e4c88c2604
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-10-30 22:28:08 +0530

    edit listing api

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -1,25 +1,44 @@
 import Listing from '../models/Listing.models.js'
 import { errorhandler } from '../utils/error.js'
 
-export const createlisting=async(req,res,next)=>{
-    try {
-        const listing= await Listing.create(req.body)
-        res.status(201).json(listing)
-    } catch (error) {
-        next(error)
-    }
-}
-export const deletelisting=async(req,res,next)=>{
-  const listing=await Listing.findById(req.params.id)
-  if(!listing){
-    return next(errorhandler(404,"listing not found"))
+export const createlisting = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body)
+    res.status(201).json(listing)
+  } catch (error) {
+    next(error)
   }
-  if(req.user.id !== listing.userRef){
-    return next(errorhandler(404,"User not found"))
+}
+export const deletelisting = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id)
+  if (!listing) {
+    return next(errorhandler(404, "listing not found"))
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorhandler(404, "User not found"))
   }
   try {
     await Listing.findByIdAndDelete(req.params.id)
     res.status(200).json("User deleted successfully")
+  } catch (error) {
+    next(error)
+  }
+}
+export const updatelisting = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id)
+  if (!listing) {
+    return next(errorhandler(404, "Listings not found"))
+  }
+  if ( req.user.id !==  listing.userRef) {
+    return next(errorhandler(404, "User doesnot exist"))
+  }
+  try {
+    await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+    res.status(200).json(listing)
   } catch (error) {
     next(error)
   }
--- api/routes/listing.router.js
+++ api/routes/listing.router.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import {createlisting, deletelisting} from '../controller/listing.controller.js'
+import {createlisting, deletelisting,updatelisting} from '../controller/listing.controller.js'
 import  {verifyUser}  from "../utils/verifyUser.js";
 
 const router =express.Router();
 
 router.post('/createlisting',createlisting)
-
-
 router.delete("/delete/:id",verifyUser,deletelisting)
+router.post("/update/:id",verifyUser,updatelisting)
 export default router;
