980023fbaef000327f5026e9efa346ada4d95162
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-11-03 23:53:02 +0530

    search api

--- api/routes/listing.router.js
+++ api/routes/listing.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {createlisting, deletelisting,updatelisting,getlisting} from '../controller/listing.controller.js'
+import {createlisting, deletelisting,updatelisting,getlisting,getlistings} from '../controller/listing.controller.js'
 import  {verifyUser}  from "../utils/verifyUser.js";
 
 const router =express.Router();
@@ -8,4 +8,5 @@ router.post('/createlisting',createlisting)
 router.delete("/delete/:id",verifyUser,deletelisting)
 router.post("/update/:id",verifyUser,updatelisting)
 router.get("/get/:id",getlisting)
+router.get("/get",getlistings)
 export default router;
