9b7a2267ecc8172a55c932dd717b3b1672e0a384
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-11-07 20:50:49 +0530

    searchlisting

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -54,7 +54,6 @@ export const getlisting = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-
 }
 export const getlistings = async (req, res, next) => {
   try {
@@ -79,7 +78,6 @@ export const getlistings = async (req, res, next) => {
     const searchTerm = req.query.searchTerm || " "
     const sort = req.query.sort || "createdAt"
     const order = req.query.order || "desc"
-
     const listings = await Listing.find({
       name: { $regex: searchTerm, $options: 'i' },
       offer,
@@ -91,7 +89,6 @@ export const getlistings = async (req, res, next) => {
       .limit(limit)
       .skip(startIndex);
       return res.status(200).json(listings)
-
   } catch (error) {
     next(error)
   }
